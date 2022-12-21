0d1e7d855dc93709e875efd82e12197304a63ea7
Jyot <10yotpatel@gmail.com>
2022-12-21 14:07:03 +0530

    some chages made

--- tp/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#burger").addEventListener("click",() => {
-    document.querySelector("#mobile-menu").classList.toggle("hidden");
-    document.querySelectorAll("#burger svg")[1].classList.toggle("hidden");
-    document.querySelectorAll("#burger svg")[0].classList.toggle("hidden");
-})
-
-document
